5fda19caea13ae1106ff7084e65e355681db2e10
Gamesh <justinasu@gmail.com>
2013-11-08 14:58:09 +0200

    Removed additional whitespace
    Breaks in propel 1.6.9

--- ArrayAccessBehavior.php
+++ ArrayAccessBehavior.php
@@ -26,8 +26,8 @@ class ArrayAccessBehavior extends Behavior
 	
 	public function objectFilter(&$script)
 	{
-		$pattern = '/abstract class (\w+) extends (\w+)  implements (\w+)/i';
-		$replace = 'abstract class ${1} extends ${2}  implements ${3}, ArrayAccess';
+		$pattern = '/abstract class (\w+) extends (\w+) implements (\w+)/i';
+		$replace = 'abstract class ${1} extends ${2} implements ${3}, ArrayAccess';
 		$script = preg_replace($pattern, $replace, $script);
 	}
 	
